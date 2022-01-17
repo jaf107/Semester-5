91023c2963084ffe24948bff8e54afc89fb6ef04
jaf107 <48786629+jaf107@users.noreply.github.com>
2022-01-16 23:23:09 -0800

    Add files via upload
    First Commit

---
+++ DBMS/StringCompare.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string a = "abs";
+    string b = "adcs";
+
+    cout << a.compare(b) ;
+    // Returns positive value  (1)  if a is larger
+    // Returns negative value (-1) if a is smaller
+    // Returns 0 if equal
+
+    return 0;
+}
+
