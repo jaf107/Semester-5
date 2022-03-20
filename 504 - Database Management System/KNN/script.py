26477ad8efbb05f2b53f70fc514ea0e9555f3b1c
jaf107 <48786629+jaf107@users.noreply.github.com>
2022-03-20 20:09:06 +0600

    Testing out script

--- 504 - Database Management System/KNN/script.py
+++ 504 - Database Management System/KNN/script.py
@@ -1,6 +1,20 @@
-from PIL import Image 
+import numpy
+from PIL import Image
 
-img = Image.open('dataset/sample1/snow1.jpg')
-
-print(img.format)
+img = Image.open('dataset/sample1/snow1.jpg', 'r')
+width, height = img.size
+channels = 3
+pixel_values = list(img.getdata())
+print(img.size)
+# print(img.format)
 print(img.mode)
+
+for i in range(9):
+    print(pixel_values[i])
+pixel_values = numpy.array(pixel_values).reshape((width, height, channels))
+
+print("AFTER ORGANIZING")
+
+for i in range(3):
+    print(pixel_values[i])
+# print(pixel_values)
