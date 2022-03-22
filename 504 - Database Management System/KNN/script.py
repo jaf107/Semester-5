4eee7ce4baa0be76fd8514084dade2948df167fe
jaf107 <48786629+jaf107@users.noreply.github.com>
2022-03-22 21:05:10 +0600

    Update script.py

--- 504 - Database Management System/KNN/script.py
+++ 504 - Database Management System/KNN/script.py
@@ -1,18 +1,20 @@
 import numpy
 from PIL import Image
 
-img = Image.open('dataset/sample1/snow1.jpg', 'r')
+img = Image.open('dataset/all_sample/s1.jpg', 'r')
+test_img = Image.open('dataset/test/test_image_1.jpg')
 width, height = img.size
+width2, h2 = test_img.size
+
+print(img.size, " ", test_img.size)
 channels = 3
 pixel_values = list(img.getdata())
 print(img.size)
 # print(img.format)
 print(img.mode)
 
-for i in range(width):
-    for j in range(3):
-        # print(i, j)
-        print(i, j, pixel_values[i][j])
+# for i in range(width):
+    # print(pixel_values[i])
     # print('\n')
 # pixel_values = numpy.array(pixel_values).reshape((width, height, channels))
 
